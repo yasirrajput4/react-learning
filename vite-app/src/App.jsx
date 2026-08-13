b786a7d6005f0739a2f6445bb68bcfd28579a7b9
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-14 01:00:02 +0530

    refactor: move component styles to global CSS file

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline ">Hello, Vite!</h1>;
+  return (
+    <>
+      <h1>Hello, Vite!</h1>
+    </>
+  );
 };
 export default App;
