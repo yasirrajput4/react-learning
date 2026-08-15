280c2126e64a3f13c6504d1dde3e76fb44d5125b
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 01:45:47 +0530

    feat(contact): implement contact form using React 19 form actions

--- country-project/src/pages/Contact.jsx
+++ country-project/src/pages/Contact.jsx
@@ -1,5 +1,47 @@
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
+const Contact = () => {
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="enter your name"
+            name="username"
+            required
+            autoComplete="off"
+          />
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter you email"
+            name="email"
+            required
+            autoComplete="off"
+          />
+
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            name="message"
+            required
+            autoComplete="off"
+          ></textarea>
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
