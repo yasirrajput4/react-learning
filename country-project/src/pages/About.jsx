888d0fa9b74612eb0438eeb8b88b82c755a27e7b
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 01:05:14 +0530

    feat(about): render dynamic country facts from JSON data

--- country-project/src/pages/About.jsx
+++ country-project/src/pages/About.jsx
@@ -1,5 +1,40 @@
-function About() {
-  return <h1>About Page</h1>;
-}
+import countryFacts from "../api/countryData.json";
 
+const About = () => {
+  return (
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the Interesting Facts
+        <br />
+        we’re proud of
+      </h2>
+
+      <div className="gradient-cards">
+        {countryFacts.map((country) => {
+          const { id, countryName, capital, population, interestingFact } =
+            country;
+          return (
+            <div className="card" key={id}>
+              <div className="container-card bg-blue-box">
+                <p className="card-title">{countryName}</p>
+                <p>
+                  <span className="card-description">Capital:</span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Fact:</span>
+                  {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 export default About;
