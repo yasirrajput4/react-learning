f71a47d996467dda5a2226411776dcbec48d54f6
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 00:50:14 +0530

    feat(header): add layout header with navigation links

--- country-project/src/components/UI/Headers.jsx
+++ country-project/src/components/UI/Headers.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
-function Headers() {
+const Headers = () => {
+  const [show, setShow] = useState(false);
+
+  const handleButtonToggle = () => {
+    return setShow(!show);
+  };
+
   return (
-    <>
-      <h1>Header Page</h1>
-    </>
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="Logo">
+            <NavLink to="/">
+              <h1>WorldAtlas</h1>
+            </NavLink>
+          </div>
+
+          <nav className={show ? "menu-mobile" : "menu-web"}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/country">Country</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="ham-menu">
+            <button onClick={handleButtonToggle}>
+              <GiHamburgerMenu />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
   );
-}
+};
 
 export default Headers;
