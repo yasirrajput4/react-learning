5a07bc2a1ea8a3cdb11394c3f47a0f4d8a2d2a6c
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 14:13:19 +0530

    feat(loader):  component complete

---
+++ country-project/src/components/UI/Loader.jsx
@@ -0,0 +1,11 @@
+import "../../App.css";
+
+function Loader() {
+  return (
+    <div className="container loader-section">
+      <div className="loader"></div>;
+    </div>
+  );
+}
+
+export default Loader;
