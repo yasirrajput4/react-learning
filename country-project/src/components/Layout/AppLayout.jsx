0a8f487641022ed9aa99ac3bba681b3c39291f53
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 21:13:18 +0530

    style(app-layout): refactor main layout wrapper using Tailwind CSS

--- country-project/src/components/Layout/AppLayout.jsx
+++ country-project/src/components/Layout/AppLayout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Footers from "../UI/Footers.jsx";
-import Headers from "../UI/Headers.jsx"
+import Headers from "../UI/Headers.jsx";
+
 const AppLayout = () => {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Headers />
-      <Outlet />
+      <div className="flex-1">
+        <Outlet />
+      </div>
       <Footers />
-    </>
+    </div>
   );
 };
 
