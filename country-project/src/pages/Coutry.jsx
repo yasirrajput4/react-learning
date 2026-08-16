6eb49038cdb36156acb1c1fe54e8d564807d2b1f
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 14:14:32 +0530

    refactor(Coutry.jsx): update country page data fetching logic and error state

--- country-project/src/pages/Coutry.jsx
+++ country-project/src/pages/Coutry.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import Loader from "../components/UI/Loader";
+
 function Coutry() {
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    startTransition(async () => {
+      try {
+        const res = await getCountryData();
+        setCountries(res.data.data.objects);
+      } catch (err) {
+        setError(err);
+      }
+    });
+  }, []);
+
+  if (isPending) return <Loader />;
+  if (error) return <h1>Something went wrong. Please try again later.</h1>;
+
   return <h1>Country Page</h1>;
 }
 
