f4085bace28c3c31c8de18002b034fa7878a0587
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-16 16:43:50 +0530

    feat(router): add dynamic route country/:id for CountryDetails page

--- country-project/src/App.jsx
+++ country-project/src/App.jsx
@@ -8,6 +8,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Country from "./pages/Coutry";
 import ErrorPage from "./pages/ErrorPage";
+import CountryDetails from "./components/Layout/CountryDetails";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
       {
         path: "country",
         element: <Country />,
+      },
+      {
+        path: "country/:id",
+        element: <CountryDetails />,
       },
       {
         path: "contact",
